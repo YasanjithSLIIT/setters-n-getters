b25590977ec58fa1563cc8c442ef75710803533f
Yasanjith Dharmappriya <IT20112264@my.sliit.lk>
2020-08-29 06:44:41 +0000

    Initial commit

---
+++ Date.h
@@ -0,0 +1,19 @@
+
+
+ class Date{
+
+    private:
+      int day;
+      int month;
+      int year;
+
+    public:
+      void setDay(int d);
+      void setMonth(int m);
+      void setYear(int y);
+      int getDay();
+      int getMonth();
+      int getYear();
+
+
+ };
---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include "Date.h"
+using namespace std;
+
+
+
+ 
+
+
+int main() {
+  int mday, mMonth, myear;
+
+  cout<<"Input Day:";
+  cin>>mday; 
+  cout<<"Input month:"; 
+  cin >> mMonth;
+  cout<<"Input year:";
+  cin >> myear;
+
+  Date myDate;
+  myDate.setDay(mday);
+  myDate.setMonth(mMonth);
+  myDate.setYear(myear);
+
+  cout<<myDate.getDay()<<"/"<<myDate.getMonth()<<"/"<<myDate.getYear()<<endl;
+
+  return 0;
+
+}
